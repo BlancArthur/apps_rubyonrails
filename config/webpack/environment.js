4dd48c3551305373d4f2d2263205eff29cb61641
Arthur <blancarthur33170@gmail.com>
2021-05-07 13:07:48 +0200

    remove from webpacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,15 +1,3 @@
-// This was already here.
 const { environment } = require('@rails/webpacker')
 
-// These are the lines you need to add.
-const webpack = require('webpack')
-	environment.plugins.append('Provide',
-	new webpack.ProvidePlugin({
-		$: 'jquery',
-		jQuery: 'jquery',
-		Popper: ['popper.js', 'default']
-	})
-)
-
-// This was also here already.
 module.exports = environment
