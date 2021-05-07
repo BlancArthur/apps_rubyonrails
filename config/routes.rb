95a4a864859c397aaab5b641042c102d0cbc2deb
Arthur <blancarthur33170@gmail.com>
2021-05-07 14:45:25 +0200

    stylsheet update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :tableaus
   get 'pages/home'
+  root 'pages#home'
   resources :oeuvres
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
